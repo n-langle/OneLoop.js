1e345b6d44d792084718d2eaa05cd5d7e318754f
n-langle <nicolas.langle@hotmail.fr>
2021-04-05 12:15:48 +0200

    feat (Tween) : better reverse logic

--- src/class/Tween.js
+++ src/class/Tween.js
@@ -5,41 +5,50 @@ import easings from '../object/easings';
 
 
 function Tween(options) {
-	this._startValue = 0;
-	MainLoopEntry.call(this, assign({}, Tween.defaults, options));
+	var settings = assign({}, Tween.defaults, options);
+
+	this._range = 1;
+	this._executed = 0;
+	this._direction = settings.reverse ? 1 : 0;
+
+	MainLoopEntry.call(this, settings);
 }
 
 Tween.defaults = {
 	duration: 1000,
 	easing: 'linear',
 	loop: 0,
-	yoyo: false
+	reverse: false
 };
 
 assign(Tween.prototype, 
 	MainLoopEntry.prototype, {
 
 	start: function(delay) {
-		return MainLoopEntry.prototype.start.call(this, delay, this._startValue);
+		
+		if (this.reverse) {
+			this._range = compute[this._direction](this._executed);
+			this._direction = (this._direction + 1) % 2;
+		}
+
+		return MainLoopEntry.prototype.start.call(this, delay, 1 - this._range);
 	},
 
 	update: function(timestamp, tick) {
-		var e = easings[this.easing]((timestamp - this._startTime) / this.duration),
-			p = this._startValue === 0 ? e : 1 - e;
+		var result = (easings[this.easing]((timestamp - this._startTime) / (this.duration * this._range)) * this._range) + 1 - this._range,
+			percent = compute[this._direction](result);
 
-		this.onUpdate(timestamp, tick, p);
+		this._executed = percent;
+
+		this.onUpdate(timestamp, tick, percent);
 		return this;
 	},
 
 	complete: function(timestamp, tick) {
-		var lastValue = this._startValue === 0 ? 1 : 0;
+		var lastValue = (this._direction + 1) % 2;
 
 		this.onUpdate(timestamp, tick, lastValue);
 		this.onComplete(timestamp, tick, lastValue);
-
-		if (this.yoyo) {
-			this._startValue = lastValue;
-		}
 
 		if (this.loop > 0) {
 			this.loop--;
@@ -50,8 +59,19 @@ assign(Tween.prototype,
 	},
 
 	needsUpdate: function(timestamp) {
-		return timestamp - this._startTime < this.duration;
+		return timestamp - this._startTime < this.duration * this._range;
 	}
 });
+
+var compute = [
+	// forward
+	function(value) {
+		return value;
+	},
+	// backward
+	function(value) {
+		return 1 - value;
+	}
+];
 
 export default Tween;
