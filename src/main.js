25d56fe11b1749237935ee8b6c4431f1b85b5784
n-langle <nlangle@beapi.fr>
2022-05-05 10:06:23 +0200

    refactor (main.js) : remove ScrollObserverEntry from export

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import easings from './object/easings';
 import MainLoopEntry from './class/MainLoopEntry';
 import Tween from './class/Tween';
 import ScrollObserver from './class/ScrollObserver';
-import ScrollObserverEntry from './class/ScrollObserverEntry';
 import ThrottledEvent from './class/ThrottledEvent';
 
-export { easings, MainLoopEntry, Tween, ScrollObserver, ScrollObserverEntry, ThrottledEvent };
+export { easings, MainLoopEntry, Tween, ScrollObserver, ThrottledEvent };
