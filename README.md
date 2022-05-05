ace83da522e4aa2d08093a3b2fb2ab62e7e5b9c2
n-langle <nlangle@beapi.fr>
2022-05-05 12:56:12 +0200

    feat (OneLoop) : add build folder, add readme

---
+++ README.md
@@ -0,0 +1,251 @@
+OneLoop.js
+========
+
+The aim of the project is to create an easy to use, lightweight, cross-browser, animation library. The following documentation is not exhaustive, but provide the basic informations to use the library. Take a look at the code if you want to know more.
+
+### ScrollObserver ###
+
+This code will create a scroll observer :
+
+```javascript
+import { ScrollObserver } from './build/OneLoop.min.js';
+
+const scrollObserver = new ScrollObserver();
+
+scrollObserver.observe('.css-selector', {
+    onVisible: function(scrollInformations, percentRTW, percentRTE) {
+        // do something when element is visible
+    }
+});
+```
+
+#### Options ####
+```javascript
+const scrollObserver = new ScrollObserver({
+    scrollDivider: 2, // default: 1, smooth the scroll value
+});
+```
+
+#### Methods ####
+```javascript
+/**
+ * observe
+ * @param element = selector, NodeList, NodeElement
+ * @param options = ScrollObserverEntry options
+ * @return this 
+ */
+scrollObserver.observe(element, {
+    children: '.css-selector', // selector, NodeList, NodeElement
+    onVisibilityStart: function(scrollInformations, percentRTW, percentRTE) {
+        // your code ...
+    },
+    onVisible: function(scrollInformations, percentRTW, percentRTE) {
+        /**
+         * Available for all callbacks :
+         * 
+         * this.element = current observed node element
+         * this.children = NodeList selected by the css selector inside the element
+         * 
+         * scrollInformations = { 
+         *      y
+         *      deltaY
+         *      directionY
+         * }
+         * percentRTW = percent of distance covered by the element inside the window (Relative To Window)
+         * percentRTE = percent of distance covered by the element from his start and end position (Relative To Element, usefull for elements at the document top and bottom)
+         */
+    },
+    onVisibilityEnd: function(scrollInformations, percentRTW, percentRTE) {
+        // your code ...
+    },
+    onAlways: function(scrollInformations, percentRTW, percentRTE) {
+        // your code ...
+    }
+});
+
+/**
+ * unobserve
+ * @param element = selector, NodeList, NodeElement
+ * @return this 
+ */
+scrollObserver.unobserve(element);
+
+/**
+ * destroy
+* @return undefined
+ */
+scrollObserver.destroy();
+```
+
+### Tween ###
+
+This code will create a tween :
+
+```javascript
+import { Tween } from './build/OneLoop.min.js';
+
+new Tween({
+    onUpdate: function(timestamp, tick, percent) {
+        // your code ...
+    }
+});
+```
+
+#### Options ####
+```javascript
+const tween = new Tween({
+    delay: 500,             // default: 0,
+    duration: 500,          // default: 1000,
+    easing: 'easeInCubic',  // default: 'linear', see easings below for available functions
+    loop: 1000,             // default: 0,
+    reverse: true,          // default: false, reverse the animation at each loop
+    autoStart: false,       // default: true
+    onStart: function(timestamp, tick, percent) {
+        // your code ...
+    },
+    onUpdate: function(timestamp, tick, percent) {
+        // your code ...
+    },
+    onComplete: function(timestamp, tick, percent) {
+        // your code ...
+    },
+    onStop: function() {
+        // your code ...
+    }
+});
+```
+
+#### Methods ####
+```javascript
+/**
+ * start (reverse at each call if option reverse is set to true)
+ * @param delay = override the option delay if needed
+ * @return this 
+ */
+tween.start(delay);
+
+/**
+ * stop
+ * @return this 
+ */
+tween.stop();
+```
+
+### ThrottledEvent ###
+
+This code will create a throttled/debounced event :
+
+```javascript
+import { ThrottledEvent } from './build/OneLoop.min.js';
+
+const resize = new ThrottledEvent(window, 'resize');
+
+resize.add('resizestart', function(event) {
+    // your code when window resize start
+});
+
+resize.add('resize', function(event) {
+    // your code when window is resized
+});
+
+resize.add('resizeend', function(event) {
+    // your code when window resize end
+});
+```
+
+If you want create only one event for all your script, you can use it as a singleton by calling the static method get instance :
+```javascript
+import { ThrottledEvent } from './build/OneLoop.min.js';
+
+const resize1 = ThrottledEvent.getInstance(window, 'scroll');
+
+// later in your script
+const resize2 = ThrottledEvent.getInstance(window, 'scroll'); // this will return the instance resize1
+```
+
+#### Options ####
+```javascript
+/**
+ * constructor
+ * @param element = NodeElement
+ * @param eventType
+ * @return this 
+ */
+const throttledEvent = new ThrottledEvent(element, eventType);
+```
+
+#### Methods ####
+```javascript
+/**
+ * add
+ * @param eventPhase = event, eventstart, eventend
+ * @param callback
+ * @return this 
+ */
+throttledEvent.add(eventPhase, callback);
+
+/**
+ * remove
+ * @param eventPhase = event, eventstart, eventend
+ * @param callback
+ * @return this 
+ */
+throttledEvent.remove(eventPhase, callback);
+
+/**
+ * destroy
+ * @return undefined
+ */
+throttledEvent.destroy();
+```
+
+### easings ###
+
+Set of easings functions, credit goes to [ai/easings.net](https://github.com/ai/easings.net/blob/master/src/easings/easingsFunctions.ts)
+
+```javascript
+import { Tween, easings } from './build/OneLoop.min.js';
+
+const tween = new Tween({
+    easing: 'linear',
+    onUpdate: function(timestamp, tick, percent) {
+        const value1 = easings.easeInCubic(percent);
+        const value2 = easings.easeOutExpo(percent);
+
+        // your code ...
+    }
+});
+
+// list of available easings functions :
+easings.linear();
+easings.easeInQuad();
+easings.easeOutQuad();
+easings.easeInOutQuad();
+easings.easeInCubic();
+easings.easeOutCubic();
+easings.easeInOutCubic();
+easings.easeInQuart();
+easings.easeOutQuart();
+easings.easeInOutQuart();
+easings.easeInQuint();
+easings.easeOutQuint();
+easings.easeInOutQuint();
+easings.easeInSine();
+easings.easeOutSine();
+easings.easeInOutSine();
+easings.easeInExpo();
+easings.easeOutExpo();
+easings.easeInOutExpo();
+easings.easeInCirc();
+easings.easeOutCirc();
+easings.easeInOutCirc();
+easings.easeInBack();
+easings.easeOutBack();
+easings.easeInOutBack();
+easings.easeInElastic();
+easings.easeOutElastic();
+easings.easeInOutElastic();
+easings.easeInBounce();
+easings.easeOutBounce();
+easings.easeInOutBounce();
+```
---
+++ build/OneLoop.js
@@ -0,0 +1,745 @@
+/**
+* @license
+* Copyright 2022 OneLoop.js
+* Author: Nicolas Langle
+* Repository: https://github.com/n-langle/OneLoop.js
+* Version: 2.0.0
+* SPDX-License-Identifier: MIT
+* 
+* Credit for easing functions goes to : https://github.com/ai/easings.net/blob/master/src/easings/easingsFunctions.ts
+*/
+var easings = (function() {
+    var pow = Math.pow;
+    var sqrt = Math.sqrt;
+    var sin = Math.sin;
+    var cos = Math.cos;
+    var PI = Math.PI;
+    var c1 = 1.70158;
+    var c2 = c1 * 1.525;
+    var c3 = c1 + 1;
+    var c4 = (2 * PI) / 3;
+    var c5 = (2 * PI) / 4.5;
+
+    function bounceOut(x) {
+        var n1 = 7.5625;
+        var d1 = 2.75;
+
+        if (x < 1 / d1) {
+            return n1 * x * x;
+        } else if (x < 2 / d1) {
+            return n1 * (x -= 1.5 / d1) * x + 0.75;
+        } else if (x < 2.5 / d1) {
+            return n1 * (x -= 2.25 / d1) * x + 0.9375;
+        } else {
+            return n1 * (x -= 2.625 / d1) * x + 0.984375;
+        }
+    }
+
+    return {
+        linear: function(x) {
+            return x;
+        },
+        easeInQuad: function(x) {
+            return x * x;
+        },
+        easeOutQuad: function(x) {
+            return 1 - (1 - x) * (1 - x);
+        },
+        easeInOutQuad: function(x) {
+            return x < 0.5 ? 2 * x * x : 1 - pow(-2 * x + 2, 2) / 2;
+        },
+        easeInCubic: function(x) {
+            return x * x * x;
+        },
+        easeOutCubic: function(x) {
+            return 1 - pow(1 - x, 3);
+        },
+        easeInOutCubic: function(x) {
+            return x < 0.5 ? 4 * x * x * x : 1 - pow(-2 * x + 2, 3) / 2;
+        },
+        easeInQuart: function(x) {
+            return x * x * x * x;
+        },
+        easeOutQuart: function(x) {
+            return 1 - pow(1 - x, 4);
+        },
+        easeInOutQuart: function(x) {
+            return x < 0.5 ? 8 * x * x * x * x : 1 - pow(-2 * x + 2, 4) / 2;
+        },
+        easeInQuint: function(x) {
+            return x * x * x * x * x;
+        },
+        easeOutQuint: function(x) {
+            return 1 - pow(1 - x, 5);
+        },
+        easeInOutQuint: function(x) {
+            return x < 0.5 ? 16 * x * x * x * x * x : 1 - pow(-2 * x + 2, 5) / 2;
+        },
+        easeInSine: function(x) {
+            return 1 - cos((x * PI) / 2);
+        },
+        easeOutSine: function(x) {
+            return sin((x * PI) / 2);
+        },
+        easeInOutSine: function(x) {
+            return -(cos(PI * x) - 1) / 2;
+        },
+        easeInExpo: function(x) {
+            return x === 0 ? 0 : pow(2, 10 * x - 10);
+        },
+        easeOutExpo: function(x) {
+            return x === 1 ? 1 : 1 - pow(2, -10 * x);
+        },
+        easeInOutExpo: function(x) {
+            return x === 0
+                ? 0
+                : x === 1
+                ? 1
+                : x < 0.5
+                ? pow(2, 20 * x - 10) / 2
+                : (2 - pow(2, -20 * x + 10)) / 2;
+        },
+        easeInCirc: function(x) {
+            return 1 - sqrt(1 - pow(x, 2));
+        },
+        easeOutCirc: function(x) {
+            return sqrt(1 - pow(x - 1, 2));
+        },
+        easeInOutCirc: function(x) {
+            return x < 0.5
+                ? (1 - sqrt(1 - pow(2 * x, 2))) / 2
+                : (sqrt(1 - pow(-2 * x + 2, 2)) + 1) / 2;
+        },
+        easeInBack: function(x) {
+            return c3 * x * x * x - c1 * x * x;
+        },
+        easeOutBack: function(x) {
+            return 1 + c3 * pow(x - 1, 3) + c1 * pow(x - 1, 2);
+        },
+        easeInOutBack: function(x) {
+            return x < 0.5
+                ? (pow(2 * x, 2) * ((c2 + 1) * 2 * x - c2)) / 2
+                : (pow(2 * x - 2, 2) * ((c2 + 1) * (x * 2 - 2) + c2) + 2) / 2;
+        },
+        easeInElastic: function(x) {
+            return x === 0
+                ? 0
+                : x === 1
+                ? 1
+                : -pow(2, 10 * x - 10) * sin((x * 10 - 10.75) * c4);
+        },
+        easeOutElastic: function(x) {
+            return x === 0
+                ? 0
+                : x === 1
+                ? 1
+                : pow(2, -10 * x) * sin((x * 10 - 0.75) * c4) + 1;
+        },
+        easeInOutElastic: function(x) {
+            return x === 0
+                ? 0
+                : x === 1
+                ? 1
+                : x < 0.5
+                ? -(pow(2, 20 * x - 10) * sin((20 * x - 11.125) * c5)) / 2
+                : (pow(2, -20 * x + 10) * sin((20 * x - 11.125) * c5)) / 2 + 1;
+        },
+        easeInBounce: function(x) {
+            return 1 - bounceOut(1 - x);
+        },
+        easeOutBounce: bounceOut,
+        easeInOutBounce: function(x) {
+            return x < 0.5
+                ? (1 - bounceOut(1 - 2 * x)) / 2
+                : (1 + bounceOut(2 * x - 1)) / 2;
+        },
+    };
+
+})();
+
+var entries = [],
+    raf = null;
+
+var mainLoop = {
+    add: function(entry) {
+        if (entries.indexOf(entry) === -1) {
+            entries.push(entry);
+            
+            if (raf === null) {
+                this.start();
+            }
+        }
+
+        return this;
+    },
+
+    remove: function(entry) {
+        var index = entries.indexOf(entry);
+        if (index > -1) {
+            entries.splice(index, 1)[0].stop();
+        }
+
+        return this;
+    },
+
+    start: function() {
+        var lastTime;
+
+        if (raf === null) { 
+
+            lastTime = performance.now();
+            
+            function loop(timestamp) {
+                var tick = (timestamp - lastTime) / (1000 / 60),
+                    i;
+                    
+                for (i = 0; i < entries.length; i++) {					
+                    if (entries[i].needsUpdate(timestamp)) {
+                        entries[i].update(timestamp, tick);
+                    } else {
+                        entries.splice(i, 1)[0].complete(timestamp, tick);
+                        i--;
+                    }
+                }
+
+                if (entries.length) {
+                    lastTime = timestamp;
+                    raf = requestAnimationFrame(loop);
+                } else {
+                    raf = null;
+                }
+            }
+
+            raf = requestAnimationFrame(loop);
+        }
+
+        return this;
+    },
+
+    stop: function() {
+        cancelAnimationFrame(raf);
+        raf = null;
+        return this;
+    },
+
+    destroy: function() {
+        entries.length = 0;
+        this.stop();
+    }
+};
+
+function assign() {
+    var args = arguments,
+        rt = args[0],
+        i, prop;
+        
+
+    for (i = 1; i < args.length; i++) {
+        for (prop in args[i]) {
+            if (typeof args[i][prop] !== 'undefined') {
+                rt[prop] = args[i][prop];
+            }
+        }
+    }
+
+    return rt
+}
+
+function noop(){}
+
+function MainLoopEntry(options) {
+   assign(this, MainLoopEntry.defaults, options);
+
+   this._startTime = 0;
+
+   if (this.autoStart) {
+      this.start();
+   }
+}
+
+MainLoopEntry.defaults = {
+   delay: 0,
+   onStart: noop,
+   onUpdate: noop,
+   onStop: noop,
+   onComplete: noop,
+   autoStart: true
+};
+
+assign(MainLoopEntry.prototype, {
+   start: function(delay, onStartAdditionalParameter) {
+      if (delay !== 0 && !delay) {
+         delay = this.delay;
+      }
+
+      if (delay === 0) {
+         this._startTime = performance.now();
+         mainLoop.add(this);
+         this.onStart(this._startTime, 0, onStartAdditionalParameter);
+      } else {
+         setTimeout(this.start.bind(this, 0, onStartAdditionalParameter), delay);
+      }
+
+      return this;
+   },
+
+   stop: function() {
+      mainLoop.remove(this);
+      this.onStop();
+      return this;
+   },
+
+   update: function(timestamp, tick) {
+      this.onUpdate(timestamp, tick);
+      return this;
+   },
+
+   complete: function(timestamp, tick) {
+      this.onComplete(timestamp, tick);
+      return this;
+   },
+
+   needsUpdate: function(timestamp) {
+      return true;
+   }
+});
+
+function Tween(options) {
+    var settings = assign({}, Tween.defaults, options);
+
+    this._range = 1;
+    this._executed = 0;
+    this._direction = settings.reverse ? 1 : 0;
+
+    MainLoopEntry.call(this, settings);
+}
+
+Tween.defaults = {
+    duration: 1000,
+    easing: 'linear',
+    loop: 0,
+    reverse: false
+};
+
+assign(Tween.prototype, 
+    MainLoopEntry.prototype, {
+
+    start: function(delay) {
+        
+        if (this.reverse) {
+            this._range = compute[this._direction](this._executed);
+            this._direction = (this._direction + 1) % 2;
+        }
+
+        return MainLoopEntry.prototype.start.call(this, delay, 1 - this._range);
+    },
+
+    update: function(timestamp, tick) {
+        var result = (easings[this.easing]((timestamp - this._startTime) / (this.duration * this._range)) * this._range) + 1 - this._range,
+            percent = compute[this._direction](result);
+
+        this._executed = percent;
+
+        this.onUpdate(timestamp, tick, percent);
+        return this;
+    },
+
+    complete: function(timestamp, tick) {
+        var lastValue = (this._direction + 1) % 2;
+
+        this.onUpdate(timestamp, tick, lastValue);
+        this.onComplete(timestamp, tick, lastValue);
+
+        if (this.loop > 0) {
+            this.loop--;
+            this.start();
+        }
+
+        return this;
+    },
+
+    needsUpdate: function(timestamp) {
+        return timestamp - this._startTime < this.duration * this._range;
+    }
+});
+
+var compute = [
+    // forward
+    function(value) {
+        return value;
+    },
+    // backward
+    function(value) {
+        return 1 - value;
+    }
+];
+
+function getElements (element, context) {
+    return typeof element === 'string' ? (context || document).querySelectorAll(element) : element.length >= 0 ? element : [element];
+}
+
+var instances$1 = [];
+
+function ThrottledEvent(target, eventType) {
+    MainLoopEntry.call(this, {autoStart: false});
+    
+    var events = {};
+
+    events[eventType + 'start'] = [];
+    events[eventType] = [];
+    events[eventType + 'end'] = [];
+
+    this._events = events;
+
+    this._needsUpdate = false;
+    this._reset = reset.bind(this);
+    this._onEvent = onEvent.bind(this);
+    this._timer = null;
+    this._target = target;
+    this._eventType = eventType;
+    this._event = null;
+
+    this._target.addEventListener(this._eventType, this._onEvent);
+}
+
+assign(ThrottledEvent.prototype, 
+    MainLoopEntry.prototype, {
+
+    destroy: function() {
+        var i;
+
+        for (i = 0; i < instances$1.length; i++) {
+            if (instances$1[i].instance === this) {
+                instances$1.splice(i, 1);
+            }
+        }
+
+        this._target.removeEventListener(this._eventType, this._onEvent);
+    },
+
+    add: function(when, callback) {
+        if (this._events[when].indexOf(callback) === -1) {
+            this._events[when].push(callback);
+        }
+
+        return this;
+    },
+
+    remove: function(when, callback) {
+        var index = this._events[when].indexOf(callback);
+        if (index > -1) {
+            this._events[when].splice(index, 1);
+        }
+        return this;
+    },
+
+    update: function(timestamp, tick) {
+        dispatch(this._events[this._eventType], this._event);
+        return this;
+    },
+
+    complete: function(timestamp, tick) {
+        dispatch(this._events[this._eventType + 'end'], this._event);
+        return this;
+    },
+
+    needsUpdate: function(timestamp) {
+        return this._needsUpdate;
+    }
+});
+
+ThrottledEvent.getInstance = function(target, eventType) {
+    var instance, i;
+
+    for (i = 0; i < instances$1.length; i++) {
+        if (instances$1[i].eventType === eventType && instances$1[i].target === target) {
+            instance = instances$1[i].instance;
+        }
+    }
+
+    if (!instance) {
+        instance = new ThrottledEvent(target, eventType);
+        
+        instances$1.push({
+            instance: instance,
+            target: target,
+            eventType: eventType
+        });
+    }
+
+    return instance;
+};
+
+ThrottledEvent.destroy = function() {
+    while (instances$1.length) {
+        instances$1[0].instance.destroy();
+    }
+};
+
+// ----
+// utils
+// ----
+function reset() {
+    this._needsUpdate = false;
+}
+
+function dispatch(array, e) {
+    var i = 0;
+
+    for (; i < array.length; i++) {
+        array[i](e);
+    }
+}
+
+// ----
+// event
+// ----
+function onEvent(e) {
+    this._event = e;
+
+    if (!this._needsUpdate) {
+        this._needsUpdate = true;
+        this.start();
+        dispatch(this._events[this._eventType + 'start'], e);
+    }
+
+    clearTimeout(this._timer);
+    this._timer = setTimeout(this._reset, 128);
+}
+
+function ScrollObserverEntry(element, options, scroll) {
+    assign(this, ScrollObserverEntry.defaults, options);
+
+    this.element = element;
+    this._isVisible = false;
+    this.children = this.children ? getElements(this.children, this.element) : [];
+
+    this.refresh(scroll);
+}
+
+ScrollObserverEntry.defaults = {
+    children: '',
+    onVisible: noop,
+    onVisibilityStart: noop,
+    onVisibilityEnd: noop,
+    onAlways: noop
+};
+
+assign(ScrollObserverEntry.prototype, {
+    refresh: function(scroll) {
+        var bounding = this.element.getBoundingClientRect(),
+            scrollY = window.pageYOffset,
+            height = window.innerHeight;
+        
+        // start and distance Relative To Window 
+        this.distanceRTW = height + bounding.bottom - bounding.top;
+        this.startRTW = bounding.bottom - this.distanceRTW + scrollY;
+
+        // start end distance Relative To Element
+        this.startRTE = Math.max(bounding.top + scrollY - height, 0);
+        this.distanceRTE = Math.min(bounding.bottom + scrollY - this.startRTE, document.documentElement.scrollHeight - height);
+
+        this.boundingClientRect = bounding;
+
+        this.control(scroll);
+
+        return this;
+    },
+
+    control: function(scroll) {
+        var p1 = (scroll.y - this.startRTW) / this.distanceRTW,
+            p2 = (scroll.y - this.startRTE) / this.distanceRTE;
+
+        if (p1 >= 0 && p1 <= 1) {
+            if (!this._isVisible) {
+                this._isVisible = true;
+                this.onVisibilityStart.call(this, scroll, round(p1), round(p2));
+                this.onVisible.call(this, scroll, round(p1), round(p2));
+            }
+
+            this.onVisible.call(this, scroll, p1, p2);
+
+        } else if (this._isVisible) {
+            this._isVisible = false;
+            
+            this.onVisible.call(this, scroll, round(p1), round(p2));
+            this.onVisibilityEnd.call(this, scroll, round(p1), round(p2));
+        }
+
+        this.onAlways.call(this, scroll, p1, p2);
+
+        return this; 
+    }
+});
+
+function round(v) {
+    return Math.abs(Math.round(v));
+}
+
+var instances = [],
+    autoRefreshTimer = null,
+    resize = null,
+    scroll = null;
+
+function ScrollObserver(options) {
+    MainLoopEntry.call(this, assign({}, ScrollObserver.defaults, options, {autoStart: false}));
+
+    this._elements = [];
+    this._entries = [];
+    this._onScroll = this.start.bind(this);
+    this._onResize = this.refresh.bind(this);
+    this._lastScrollY = 0;
+
+    if (instances.length === 0) {
+        resize = new ThrottledEvent(window, 'resize');
+        scroll = new ThrottledEvent(window, 'scroll');
+    }
+
+    resize.add('resize', this._onResize);
+    scroll.add('scrollstart', this._onScroll);
+
+    instances.push(this);
+    ScrollObserver.startAutoRefresh();
+}
+
+ScrollObserver.defaults = {
+    scrollDivider: 1
+};
+
+assign(ScrollObserver.prototype,
+    MainLoopEntry.prototype, {
+
+    destroy: function() {
+        instances.splice(instances.indexOf(this),  1);
+
+        if (instances.length === 0) {
+            ScrollObserver.stopAutoRefresh();
+            resize.destroy();
+            scroll.destroy();
+            resize = scroll = null;
+        } else {
+            resize.remove('resize', this._onResize);
+            scroll.remove('scrollstart', this._onScroll);
+        }
+    },
+
+    observe: function(element, options) {
+        var els = getElements(element),
+            scroll = this.getScrollInfos(),
+            i;
+
+        for (i = 0; i < els.length; i++) {
+            if (this._elements.indexOf(element) === -1) {
+                this._entries.push(new ScrollObserverEntry(els[i], options, scroll));
+                this._elements.push(els[i]);
+            }
+        }
+
+        return this;	
+    },
+
+    unobserve: function(element) {
+        var els = getElements(element),
+            index, 
+            i;
+
+        for (i = 0; i < els.length; i++) {
+            index = this._elements.indexOf(element); 
+            if (index > -1) {
+                this._elements.splice(index, 1);
+                this._entries.splice(index, 1);
+            }
+        }
+
+        return this;
+    },
+
+    update: function(timestamp, tick) {
+        this.onUpdate(timestamp, tick);
+
+        var scroll = this.getScrollInfos(),
+            i; 
+
+        for (i = 0; i < this._entries.length; i++) {
+            this._entries[i].control(scroll);
+        }
+
+        this._lastScrollY = scroll.y;
+
+        return this;
+    },
+
+    needsUpdate: function(timestamp) {
+        return scroll.needsUpdate() || this.scrollDivider > 1 && Math.abs(window.pageYOffset - this._lastScrollY) > 1;
+    },
+
+    getScrollInfos: function() {
+        var y = this._lastScrollY + (window.pageYOffset - this._lastScrollY) / this.scrollDivider,
+            deltaY = y - this._lastScrollY;
+        
+        return {
+            y: y,
+            deltaY: deltaY,
+            directionY: deltaY / Math.abs(deltaY) || 0
+        }
+    },
+
+    refresh: function() {
+        var scrollInfos = this.getScrollInfos(),
+            i;
+    
+        for (i = 0; i < this._entries.length; i++) {
+            this._entries[i].refresh(scrollInfos);
+        }
+
+        return this;
+    }
+});
+
+// ----
+// utils
+// ----
+function getDocumentHeight() {
+    var html = document.documentElement,
+        body = document.body;
+
+    return Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
+}
+
+// ----
+// statics
+// ----
+ScrollObserver.autoRefreshDelay = 1000;
+
+ScrollObserver.startAutoRefresh = function() {
+    if (autoRefreshTimer === null && ScrollObserver.autoRefreshDelay !== null) {
+        var lastDocumentHeight = getDocumentHeight();
+
+        autoRefreshTimer = setInterval(function() {
+            var height = getDocumentHeight(),
+                i;
+
+            if ( height !== lastDocumentHeight) {
+                for (i = 0; i < instances.length; i++) {
+                    instances[i].refresh();
+                }
+                lastDocumentHeight = height;
+            }
+        }, ScrollObserver.autoRefreshDelay);
+    }
+    return this;
+};
+
+ScrollObserver.stopAutoRefresh = function() {
+    clearInterval(autoRefreshTimer);
+    autoRefreshTimer = null;
+    return this;
+};
+
+ScrollObserver.destroy = function() {
+    while(instances.length) {
+        instances[0].destroy();
+    }
+};
+
+export { MainLoopEntry, ScrollObserver, ThrottledEvent, Tween, easings };
