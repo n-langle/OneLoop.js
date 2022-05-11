d9725fdf7758c35eb4edcb25d47a759fb87e7fb4
n-langle <nlangle@beapi.fr>
2022-05-11 18:16:17 +0200

    feat (SplittedText) : add html entities and code support

--- src/class/SplittedText.js
+++ src/class/SplittedText.js
@@ -3,7 +3,7 @@ import ThrottledEvent from './ThrottledEvent';
 
 var instances = [],
     resize = null,
-    emojiRegexp = new RegExp('(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|[\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|[\ud83c[\ude32-\ude3a]|[\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])', 'g');
+    specialCharRegExp = new RegExp('(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|[\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|[\ud83c[\ude32-\ude3a]|[\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff]|&([a-zA-Z]{2,6}|#[0-9]{2,5});|<|>)', 'g');
 
 function SplittedText(element, options) {
     assign(this, SplittedText.defaults, options);
@@ -67,7 +67,9 @@ assign(SplittedText.prototype, {
             lastOffsetTop, offsetTop, children, i;
 
         if (this.byWord || this.byLine) {
-            element.innerHTML = split(element, ' ', this.wordWrapper);
+            element.innerHTML = split(element, ' ', function(word) {
+                return that.wordWrapper(word.replace('<', '&lt;'));
+            });
         }
 
         if (this.byLine) {
@@ -121,7 +123,7 @@ function wrapSpecialChar(element, wrap) {
         child = childNodes[i];
 
         if (child.nodeType === 3) {
-            html += child.data.replace(emojiRegexp, wrap);
+            html += child.data.replace(specialCharRegExp, wrap);
         } else if (child.nodeType === 1) {
             html += getNewOuterHTML(child, wrapSpecialChar(child, wrap));
         }
