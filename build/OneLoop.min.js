1ef8e04e44e3828b1bcb8308c74de4fcd455c6e1
Nicolas Langle <nlangle@beapi.fr>
2025-02-06 09:31:14 +0100

    feat (package.json): remove engine property

--- build/OneLoop.min.js
+++ build/OneLoop.min.js
@@ -3,7 +3,7 @@
 * Copyright 2022 OneLoop.js
 * Author: Nicolas Langle
 * Repository: https://github.com/n-langle/OneLoop.js
-* Version: 5.2.0
+* Version: 5.2.1
 * SPDX-License-Identifier: MIT
 * 
 * Credit for easing functions goes to : https://github.com/ai/easings.net/blob/master/src/easings/easingsFunctions.ts
